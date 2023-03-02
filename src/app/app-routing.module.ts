66024cf333f8c92da45706593d3c39cd9fcfa48a
Lucas Vermeulen <lucasvermeulen@gmail.com>
2023-03-02 21:19:10 +0100

    install leaflet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LeafletComponent } from './leaflet/leaflet.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: LeafletComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
