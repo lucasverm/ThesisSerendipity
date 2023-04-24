8939de7cb635d9e1aef87774c74190f79adbc1ea
Lucas Vermeulen <lucasvermeulen@gmail.com>
2023-04-24 22:48:56 +0200

    fix netlify

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GraphComponent } from './graph/graph.component';
 import { MapComponent } from './map/map.component';
 import { ParseRmlComponent } from './parse-rml/parse-rml.component';
 import { QueryComponent } from './query/query.component';
-import { GraphComponent } from './graph/graph.component';
 
 const routes: Routes = [
   { path: 'map', component: MapComponent },
   { path: 'query', component: QueryComponent },
   { path: 'parse', component: ParseRmlComponent },
   { path: 'graph', component: GraphComponent },
-  { path: '', component: ParseRmlComponent }
+  { path: '', component: GraphComponent }
 ];
 
 @NgModule({
